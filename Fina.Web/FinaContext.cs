d0a7b365c97b47cdb4a7f30b0294259256bec0cd
Seppe De Langhe <seppedelanghe17@gmail.com>
2019-05-17 20:52:59 +0200

    DbSet database classes in FinaContext

--- Fina.Web/FinaContext.cs
+++ Fina.Web/FinaContext.cs
@@ -10,10 +10,16 @@ namespace Fina.Lib.Database
     {
         public FinaContext(DbContextOptions<FinaContext> options) : base(options)
         {
-            public DbSet<users> users { get; set }
+            
         }
 
-
+        public DbSet<users> users { get; set; }
+        public DbSet<savings> savings { get; set; }
+        public DbSet<jobs> jobs { get; set; }
+        public DbSet<security> security { get; set; }
+        public DbSet<expenses> expenses { get; set; }
+        public DbSet<single_expense> single_expenses { get; set; }
+        public DbSet<incomes> incomes { get; set; }
 
     }
 }
