d319c493dbb4f613ff4eb222af61163060c1662c
Seppe De Langhe <seppedelanghe17@gmail.com>
2019-05-17 21:07:10 +0200

    setup enums and properties of users class

--- Fina.Lib/Database/users.cs
+++ Fina.Lib/Database/users.cs
@@ -6,5 +6,14 @@ namespace Fina.Lib.Database
 {
     public class users
     {
+        public enum Currencies { Euro, Dollar, Pounds, Yen }
+        public enum Countries { Belgium, Netherlands, France, Germany, Engeland, USA, Japan}
+
+        public string name { get; set; }
+        public string firstname { get; set; }
+        public Countries country { get; set; }
+        public string password { get; set; }
+        public byte age { get; set; }
+        public Currencies currency { get; set; }
     }
 }
